f6364cedb078f81f05e7c822b581597178e5d45f
vararais <varadigmaisnann@gmail.com>
2024-03-07 20:44:27 +0700

    Menambahkan BubbleSort Array

--- UrutkanData/UrutkanData.cpp
+++ UrutkanData/UrutkanData.cpp
@@ -28,6 +28,30 @@ void input() {
 }
 
 
+void bubbleSortArray() {
+	int pass = 1;
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+
+		}
+		pass = pass + 1;
+
+		cout << "\nPass" << pass - 1 << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
+		cout << endl;
+
+
+	} while (pass <= n + 1);
+}
+
 
 
 
