190acf21b9e4613b1982667e71e571ecb7ef1af5
vararais <varadigmaisnann@gmail.com>
2024-03-07 20:42:56 +0700

    Menambahkan Void Input

--- UrutkanData/UrutkanData.cpp
+++ UrutkanData/UrutkanData.cpp
@@ -5,6 +5,32 @@ int arr[20];
 int n;
 
 
+void input() {
+	while (true) {
+		cout << "Masukkan Banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+		}
+	}
+
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+
+
+
+
 
 int main()
 {
